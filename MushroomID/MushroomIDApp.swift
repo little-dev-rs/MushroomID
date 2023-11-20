38db0d99cec44d80bf021385add6b23163db2460
shardakova svetlana <shardakova.s@medlinx.online>
2023-11-20 09:10:40 +0100

    Initial Commit

---
+++ MushroomID/MushroomIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MushroomIDApp.swift
+//  MushroomID
+//
+//  Created by Svetlana Shardakova on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MushroomIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
